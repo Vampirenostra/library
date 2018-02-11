6183f4509500224cfa9a86ea6e1b4f142302c2a8
Vampirenostra <mishukd@mail.ru>
2018-02-11 23:09:05 +0200

    update

--- src/main/java/ua/vampirenostra/library/controllers/IndexController.java
+++ src/main/java/ua/vampirenostra/library/controllers/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jdbc.datasource.init.ScriptStatementFailedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,15 +29,16 @@ public class IndexController {
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
     public String createTables(Model model) {
-
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(new ClassPathResource("static/create_tables.sql"));
-
-        dbAccess(populator);
-
         String result = "Tables created!";
+        try {
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+            populator.addScript(new ClassPathResource("static/create_tables.sql"));
+            dbAccess(populator);
+            model.addAttribute("result", "Tables created!");
+        } catch (ScriptStatementFailedException e) {
+            result = "Sorry, You already have tables created";
+        }
         model.addAttribute("result", result);
-
         return "index";
     }
 
@@ -57,13 +59,14 @@ public class IndexController {
     @RequestMapping(value = "/populate", method = RequestMethod.GET)
     public String populate(Model model) {
 
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        //populator.addScript(new ClassPathResource("static/drop_index.sql"));
-        populator.addScript(new ClassPathResource("static/data.sql"));
-
-        dbAccess(populator);
-
         String result = "Database Populated!";
+        try {
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+            populator.addScript(new ClassPathResource("static/data.sql"));
+            dbAccess(populator);
+        } catch (ScriptStatementFailedException e) {
+            result = "Sorry, your database is already populated, maybe you need to clear it before re-population?";
+        }
         model.addAttribute("result", result);
 
         return "index";
